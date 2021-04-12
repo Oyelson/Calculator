cbda9eed1423e077ed5b4303f6afc97b27f284cd
Oyelson <oyegbitejohn@gmail.com>
2021-04-12 05:43:45 +0100

    Added input screen, output screen, operand and operator buttons; Implemented soft vibration of device when a button is clicked

--- app/src/main/java/com/oyegbite/calculator/MainActivity.java
+++ app/src/main/java/com/oyegbite/calculator/MainActivity.java
@@ -1,14 +1,211 @@
 package com.oyegbite.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.text.InputType;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import com.oyegbite.calculator.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+    private ActivityMainBinding binding;
+
+    private EditText inputScreen;
+    private TextView outputScreen;
+
+    private TextView deleteAll;
+    private TextView openParen;
+    private TextView closeParen;
+    private TextView divide;
+
+    private TextView seven;
+    private TextView eight;
+    private TextView nine;
+    private TextView multiply;
+
+    private TextView four;
+    private TextView five;
+    private TextView six;
+    private TextView minus;
+
+    private TextView one;
+    private TextView two;
+    private TextView three;
+    private TextView plus;
+
+    private TextView zero;
+    private TextView point;
+    private TextView equals;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // Remove notification bar
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView( R.layout.activity_main);
+//        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+
+        hideKeyboard();
+
+        StringBuilder inputString = new StringBuilder();
+
+        activateInputScreen();
+        activateOutputScreen();
+
+        activateDeleteAllButton();
+        activateOpenParenButton();
+        activateCloseParenButton();
+        activateDivideButton();
+
+        activateSevenButton();
+        activateEightButton();
+        activateNineButton();
+        activateMultiplyButton();
+
+        activateFourButton();
+        activateFiveButton();
+        activateSixButton();
+        activateMinusButton();
+
+        activateOneButton();
+        activateTwoButton();
+        activateThreeButton();
+        activatePlusButton();
+
+        activateZeroButton();
+        activatePointButton();
+        activateEqualsButton();
+
+    }
+
+    private void activateInputScreen() {
+        inputScreen = findViewById(R.id.input_screen);
+    }
+
+    private void activateOutputScreen() {
+        outputScreen= findViewById(R.id.output_screen);
+    }
+
+    private void activateDeleteAllButton() {
+        deleteAll = findViewById(R.id.delete_all);
+        deleteAll.setOnClickListener(this);
+    }
+
+    private void activateOpenParenButton() {
+        openParen = findViewById(R.id.open_paren);
+        openParen.setOnClickListener(this);
+    }
+
+    private void activateCloseParenButton() {
+        closeParen = findViewById(R.id.close_paren);
+        closeParen.setOnClickListener(this);
+    }
+
+    private void activateDivideButton() {
+        divide = findViewById(R.id.divide);
+        divide.setOnClickListener(this);
+    }
+
+    private void activateSevenButton() {
+        seven = findViewById(R.id.seven);
+        seven.setOnClickListener(this);
+    }
+
+    private void activateEightButton() {
+        eight = findViewById(R.id.eight);
+        eight.setOnClickListener(this);
+    }
+
+    private void activateNineButton() {
+        nine = findViewById(R.id.nine);
+        nine.setOnClickListener(this);
+    }
+
+    private void activateMultiplyButton() {
+        multiply = findViewById(R.id.multiply);
+        multiply.setOnClickListener(this);
+    }
+
+    private void activateFourButton() {
+        four = findViewById(R.id.four);
+        four.setOnClickListener(this);
+    }
+
+    private void activateFiveButton() {
+        five = findViewById(R.id.five);
+        five.setOnClickListener(this);
+    }
+
+    private void activateSixButton() {
+        six = findViewById(R.id.six);
+        six.setOnClickListener(this);
+    }
+
+    private void activateMinusButton() {
+        minus = findViewById(R.id.minus);
+        minus.setOnClickListener(this);
+    }
+
+    private void activateOneButton() {
+        one = findViewById(R.id.one);
+        one.setOnClickListener(this);
+    }
+
+    private void activateTwoButton() {
+        two = findViewById(R.id.two);
+        two.setOnClickListener(this);
+    }
+
+    private void activateThreeButton() {
+        three = findViewById(R.id.three);
+        three.setOnClickListener(this);
+    }
+
+    private void activatePlusButton() {
+        plus = findViewById(R.id.plus);
+        plus.setOnClickListener(this);
+    }
+
+    private void activateZeroButton() {
+        zero = findViewById(R.id.zero);
+        zero.setOnClickListener(this);
+    }
+
+    private void activatePointButton() {
+        point = findViewById(R.id.point);
+        point.setOnClickListener(this);
+    }
+
+    private void activateEqualsButton() {
+        equals = findViewById(R.id.equals);
+        equals.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        vibrate();
+    }
+
+    private void vibrate() {
+        Vibrator vibe = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        vibe.vibrate(40);
+    }
+
+    @Override
+    protected void onResume() {
+        hideKeyboard();
+        super.onResume();
+    }
+
+    private void hideKeyboard() {
     }
 }
---
+++ app/src/main/java/com/oyegbite/calculator/utils/CalculatorUtil.java
@@ -0,0 +1,259 @@
+package com.oyegbite.calculator.utils;
+
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.Map;
+import java.util.Stack;
+import java.util.List;
+import java.util.ArrayList;
+import java.lang.StringBuilder;
+
+@RequiresApi(api = Build.VERSION_CODES.R)
+public class CalculatorUtil {
+    private static String mInfixExpression;
+    private static final Map<String, Integer> operatorPrecedence =
+            Map.ofEntries(
+                    Map.entry("^", 3), // Exponent
+                    Map.entry("/", 2), // Division
+                    Map.entry("*", 2), // Multiplication
+                    Map.entry("+", 1), // Addition
+                    Map.entry("-", 1),  // Subtraction
+                    Map.entry("–", 1)  // Subtraction
+            );
+
+    public static Double calculate(String infixExpression) {
+        List<String> postfixExpression = convertInfixToPostfix(infixExpression);
+        return evaluatePostfix(postfixExpression);
+    }
+
+    public static Boolean isExpressionValid() {
+        // Todo: Check if the expression is valid based on the operator,
+        // operand and the parenthesis.
+        return true;
+    }
+
+    /**
+     * Convert infixExpression say "-2+1*5(2(+5+12/6))" to something like "0-2+1*5*(2*(0+5+12/6))"
+     *
+     * @param infixExpression
+     * @return
+     */
+    public static String includeMultiplyOperatorIfNeeded(String infixExpression) {
+        infixExpression = infixExpression.replaceAll(" ", "");
+
+        StringBuilder sb = new StringBuilder();
+        int stringLength  = infixExpression.length();
+        for (int i = 0; i < stringLength; i++) {
+            char token = infixExpression.charAt(i);
+            // char prevToken = infixExpression.charAt(i-1);
+            // char nextToken = infixExpression.charAt(i+1);
+
+            if ((i == 0 && isTokenOperator(String.valueOf(token)))
+                    || (i > 0
+                    && isTokenOperator(String.valueOf(token))
+                    && isOpenParentheses(String.valueOf(infixExpression.charAt(i-1))))) {
+                // "-2+1" --> "0-2+1"
+                // OR
+                // "2-(+2+1)+5" --> "2-(0+2+1)+5"
+                sb.append("0");
+            }
+
+            if (i > 0 && isCloseParentheses(String.valueOf(infixExpression.charAt(i-1)))
+                    && (isOpenParentheses(String.valueOf(token))
+                    || Character.isDigit(token))) {
+                // "2*(3+2)5" --> "2*(3+2)*5"
+                // "(3+2)(5+2)" --> "(3+2)*(5+2)"
+                sb.append("*");
+            }
+
+            sb.append(token);
+
+            if (i < stringLength - 1) {
+                if (Character.isDigit(token)
+                        && isOpenParentheses(String.valueOf(infixExpression.charAt(i+1)))) {
+                    sb.append("*"); // "2(3+2)+5" --> "2*(3+2)+5"
+                }
+            }
+        }
+        return sb.toString();
+    }
+
+    /**
+     * Infix expression which is the user input is in the form of
+     * "<operand> <operator> <operand>"
+     * E.g A + (B + C)
+     * A is an operand, + is an operator, B is an operand, C is an operand,
+     * (B + C) is an operand (since the addition would result to an operand)
+     *
+     * If I get an operator of lower priority, that marks the boundary of the right
+     * operand because of associativity rule
+     *
+     * 2098+300*91-84/3
+     *
+     * if I see an operator, I need to look on top of stack
+     *
+     * When you see a closing parentheses, keep popping from stack until you see an opening parentheses
+     * then stop the pop at that opening parentheses since it would mark the boundary of an operand
+     *
+     * "2098+300*91-84/3" => ["2098", "300", "91", "*", "+", "84", "3", "/", "-"]
+     *
+     * "(2098+300*(91-84))/3" => ["2098", "300", "91", "84", "-", "*", "+", "3", "/"]
+     *
+     * Assumes infixExpression is a valid expression.
+     *
+     * @param infixExpression
+     */
+    public static List<String> convertInfixToPostfix(String infixExpression) {
+        infixExpression = includeMultiplyOperatorIfNeeded(infixExpression);
+        System.out.println(infixExpression);
+        Stack<String> operatorStack = new Stack<>();
+        List<String> postfix = new ArrayList<>();
+
+        List<String> digits = new ArrayList<>();
+        for(int i = 0; i < infixExpression.length(); i++) {
+            String token = String.valueOf(infixExpression.charAt(i));
+
+            if (token.equals(" ")) continue;
+
+            if (isOpenParentheses(token)) { // If we meet a starting boundary
+                operatorStack.add(token);
+            } else if (isCloseParentheses(token)) { // If we meet a closing boundary
+                // When we see a closing boundary, and our cummulative digits is not empty,
+                // then our digits becomes a number.
+                if (!digits.isEmpty()) {
+                    postfix.add(String.join("", digits));
+                    digits = new ArrayList<>(); // reset digits to take another number
+                }
+                while (!operatorStack.isEmpty() && !isOpenParentheses(operatorStack.peek())) {
+                    postfix.add(operatorStack.pop());
+                }
+                // Remove the opening parentheses that marks the operand boundary
+                operatorStack.pop();
+
+            } else if (isTokenOperator(token)) {
+                // When we see an operator, and our cummulative digits is not empty,
+                // then our digits becomes a number.
+                if (!digits.isEmpty()) {
+                    postfix.add(String.join("", digits));
+                    digits = new ArrayList<>(); // reset digits to take another number
+                }
+                while (
+                        !operatorStack.isEmpty()
+                                && !isOpenParentheses(operatorStack.peek())
+                                && hasHigherPrecedence(operatorStack.peek(), token)) {
+                    postfix.add(operatorStack.pop());
+                }
+
+                if (i == 0 || (i > 0 && isOpenParentheses(String.valueOf(infixExpression.charAt(i-1))))) {
+                    // 1. Case of say s = "-2+1"
+                    //    (we would convert it to s = "0-2+1")
+                    // 2. Case of say s = "1-(+1+1)"
+                    //    (we would convert it to s = "1-(0+1+1)")
+                    postfix.add("0");
+                }
+                operatorStack.add(token);
+            } else {
+                // Get cummulative of digits (say we have 2098+300*91-84/3)
+                digits.add(token);
+            }
+        }
+
+        if (!digits.isEmpty()) postfix.add(String.join("", digits));
+
+        while (!operatorStack.isEmpty()) {
+            postfix.add(operatorStack.pop());
+        }
+        return postfix;
+    }
+
+    /**
+     * E.g postfixExpression: ["2", "3", "*", "5", "4", "*", "+", "9", "-"]
+     * If the token seen is an operator, it should be applied to the last
+     * two operands preceeding it and perform the operation.
+     *
+     * @param postfixExpression has its operators after its operands.
+     * @return the evaluation of the postfix expression.
+     */
+    public static Double evaluatePostfix(List<String> postfixExpression) {
+        System.out.println(postfixExpression);
+        // A token here is either an operand or an operator.
+        Stack<Double> stack = new Stack<>();
+        for (String token: postfixExpression) {
+            // If token is an operator, then it should be applied to
+            // the last two operands preceeding it and perform the operation.
+            if (isTokenOperator(token)) {
+                double secondOperand = stack.pop();
+                double firstOperand = stack.pop();
+
+                double result = performOperation(firstOperand, secondOperand, token);
+                stack.push(result);
+            } else {
+                stack.push(Double.valueOf(token));
+            }
+
+        }
+
+        // If postfix expression is valid, the stack would just have one value left.
+        return stack.peek();
+    }
+
+    private static Boolean hasHigherPrecedence(String topOperator, String newOperator) {
+        return operatorPrecedence.get(topOperator) >= operatorPrecedence.get(newOperator);
+    }
+
+    private static Boolean isTokenOperator(String token) {
+        return token.equals("+")
+                || token.equals("-")
+                || token.equals("–")
+                || token.equals("*")
+                || token.equals("/")
+                || token.equals("^");
+    }
+
+    public static Boolean isDigitEmpty(String digits) {
+        return digits.equals("") || digits == null;
+    }
+
+    public static Boolean isOpenParentheses(String token) {
+        return token.equals("(")
+                || token.equals("{")
+                || token.equals("[");
+    }
+
+    public static Boolean isCloseParentheses(String token) {
+        return token.equals(")")
+                || token.equals("}")
+                || token.equals("]");
+    }
+
+    private static Double performOperation(double firstOperand, double secondOperand, String operator) {
+        if (operator.equals("^")) {
+            return Math.pow(firstOperand, secondOperand);
+        } else if (operator.equals("*")) {
+            return firstOperand * secondOperand;
+        } else if (operator.equals("/")) {
+            return firstOperand / (double) secondOperand;
+        } else if (operator.equals("+")) {
+            return firstOperand + secondOperand;
+        } else if (operator.equals("-")) {
+            return firstOperand - secondOperand;
+        } else if (operator.equals("–")) {
+            return firstOperand - secondOperand;
+        }
+        return Double.MAX_VALUE;
+    }
+
+//    public static void main(String[] args) {
+//        // String infixExpression = "(20+5)*(500/(125*2))";
+//        // String infixExpression = "5(500/(125*2))";
+//        // String infixExpression = "  5 (2 (5 + 12 / 6))";
+//        // String infixExpression = " -2+1*5 (2 ( +5+12/6))";
+//        // String infixExpression = "2 + 3(7 – 9)";
+//        // String infixExpression = "45 + (1250 * 100) / 10";
+//        String infixExpression = "(0.+2)(5+2(7))";
+//        System.out.println(calculate(infixExpression));
+//    }
+
+}
